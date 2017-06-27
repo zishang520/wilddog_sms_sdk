a3000ffdd6d78eeea4fe75dd2623c9d6a65bc460
zishang520 <zishang520@gmail.com>
2017-06-27 09:50:08 +0800

    fix bug

--- src/Access.php
+++ src/Access.php
@@ -1,6 +1,10 @@
 <?php
 namespace luoyy\WilddogSmsSdk;
 
+use \ArrayAccess;
+use \Exception;
+use \StdClass;
+
 /**
  * 数据
  * @Author:zishang520
@@ -10,4 +14,37 @@ namespace luoyy\WilddogSmsSdk;
  */
 class Access extends StdClass implements ArrayAccess
 {
+    public function offsetExists($offset)
+    {
+        if (!is_scalar($offset)) {
+            throw new Exception("offset must be a scalar", 1);
+        }
+        return property_exists($this, $offset);
+    }
+    public function offsetGet($offset)
+    {
+        if (!is_scalar($offset)) {
+            throw new Exception("offset must be a scalar", 1);
+        }
+        if (property_exists($this, $offset)) {
+            return $this->{$offset};
+        }
+        return null;
+    }
+    public function offsetSet($offset, $value)
+    {
+        if (!is_scalar($offset)) {
+            throw new Exception("offset must be a scalar", 1);
+        }
+        $this->{$offset} = $value;
+    }
+    public function offsetUnset($offset)
+    {
+        if (!is_scalar($offset)) {
+            throw new Exception("offset must be a scalar", 1);
+        }
+        if (property_exists($this, $offset)) {
+            unset($this->{$offset});
+        }
+    }
 }
